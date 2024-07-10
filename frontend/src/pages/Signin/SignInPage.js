db3399767f83bdf4209dc4e86f355133a7ff961d
Soumen Halder <soumen.tifr2016@gmail.com>
2024-07-10 14:09:34 +0530

    attempt to do signin

---
+++ frontend/src/pages/Signin/SignInPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignInForm from './SignInForm';
+
+const SignInPage = () => {
+  return (
+    <div className="signin-page">
+      <h2>Sign In</h2>
+      <SignInForm />
+    </div>
+  );
+};
+
+export default SignInPage;
