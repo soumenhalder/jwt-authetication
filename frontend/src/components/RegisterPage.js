c527e6a0a100e4293a9291b80e5a7603719d89ea
Soumen Halder <soumen.tifr2016@gmail.com>
2024-07-13 20:45:00 +0530

    login.register page first attempt

--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './LoginPage.module.css';
+import { FaHome } from "react-icons/fa";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
@@ -18,23 +19,36 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1>Login Page</h1>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label>
-            Mobileno:
-            <input
-              type="text"
-              value={Mobileno}
-              onChange={(e) => setMobileno(e.target.value)}
-            />
-          </label>
+    <nav>
+      <div className={styles.home_button}>
+        <button className={styles.homeicon} onClick={handleHomeClick}>
+          <FaHome size={40} />
+        </button>
+      </div>
+      <div className={styles.Logincontainer}>
+        <div className={styles.Logincontent1}>
+          <div className={styles.loginImage}>
+            <img src='/images/a_Register_as_a_student banner.c445d4da.jpg' alt='reg-image' classname={styles.image} />
+          </div>
+          <div className={styles.logincontent}>
+            <h1>Register as Student</h1>
+            <form onSubmit={handleFormSubmit}>
+              <div>
+                <label>
+                  Type Your Mob Number to Register
+                  <input
+                    type="text"
+                    value={Mobileno}
+                    onChange={(e) => setMobileno(e.target.value)}
+                  />
+                </label>
+              </div>
+              <button type="submit">Register</button>
+            </form>
+          </div>
         </div>
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={handleHomeClick}>Home</button>
-    </div>
+      </div>
+    </nav>
   );
 };
 
