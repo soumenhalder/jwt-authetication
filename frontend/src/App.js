fafcf29630d06807cc6cbf35c2a730c789dd2ecc
soumenhalder <soumenhalder446@gmail.com>
2024-11-14 15:58:12 +0530

    working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import './App.css';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header_up from './components/Header_up';
 import Header_down from './components/Header_down';
-//import SignInPage from './pages/Signin/SignInPage.js'; // Import SignInPage component
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+//    const navigate = useNavigate();
+
+
     return (
         <div className="App">
             <Header_up/>  
